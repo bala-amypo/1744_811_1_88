ac05f21c8036855c89999eb7f22349477177f5fa
bala-amypo <bala@amypo.in>
2025-12-18 06:24:45 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -10,7 +10,7 @@ public class User {
     private Long id;
 
     private String name;
-    @Column(unique==true)
+    @Column(unique = true)
     private String email;
     private String password;
     private String role;
