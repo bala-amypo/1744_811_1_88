8139601cc4623fce99936cf4634c288ee2c4f837
bala-amypo <bala@amypo.in>
2025-12-19 03:44:51 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/BookingLog.java
@@ -0,0 +1,31 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookingLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Booking booking;
+
+    private String logMessage;
+
+    private LocalDateTime loggedAt;
+
+    @PrePersist
+    public void onCreate() {
+        loggedAt = LocalDateTime.now();
+    }
+}
