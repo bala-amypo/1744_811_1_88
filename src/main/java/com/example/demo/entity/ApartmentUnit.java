363f62a578c6c2ea6db7009596ef348867d1df69
bala-amypo <bala@amypo.in>
2025-12-19 03:43:29 +0000

    User commit

--- src/main/java/com/example/demo/entity/ApartmentUnit.java
+++ src/main/java/com/example/demo/entity/ApartmentUnit.java
@@ -1,56 +1,27 @@
 package com.example.demo.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "apartment_units")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ApartmentUnit {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String unitNumber;
+
     private Integer floor;
-    private Long userId;
-    public ApartmentUnit(Long id,String unitNumber,Integer floor,Long userId){
-        this.id=id;
-        @Column(unique = true)
-        this.unitNumber=unitNumber;
-        this.floor=floor;
-        this.userId=userId;
-    }
-    public ApartmentUnit() {
-    }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUnitNumber() {
-        return unitNumber;
-    }
-
-    public void setUnitNumber(String unitNumber) {
-        this.unitNumber = unitNumber;
-    }
-
-    public Integer getFloor() {
-        return floor;
-    }
-
-    public void setFloor(Integer floor) {
-        this.floor = floor;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
+    @OneToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
 }
---
+++ src/main/java/com/example/demo/entity/Facility.java
@@ -0,0 +1,24 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Facility {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    private String description;
+    private String openTime;
+    private String closeTime;
+}
