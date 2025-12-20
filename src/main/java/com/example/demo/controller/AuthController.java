3cceaffb8da74a281ee07af9f6f160aa83cde8e3
bala-amypo <bala@amypo.in>
2025-12-20 09:59:05 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -0,0 +1,34 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.User;
+import com.example.demo.security.JwtTokenProvider;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return userService.register(user);
+    }
+
+    @PostMapping("/login")
+    public Map<String, String> login(@RequestBody User user) {
+        String token = jwtTokenProvider.generateToken(user.getEmail());
+        Map<String, String> response = new HashMap<>();
+        response.put("token", token);
+        return response;
+    }
+}
