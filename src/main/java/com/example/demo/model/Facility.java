1c167bf26c141b0c1a98ebc65d5f0fba58a89e5f
bala-amypo <bala@amypo.in>
2025-12-26 03:36:38 +0000

    User commit

--- src/main/java/com/example/demo/model/Facility.java
+++ src/main/java/com/example/demo/model/Facility.java
@@ -30,18 +30,38 @@ public class Facility {
     }
 
     
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    public Long getId() { 
+        return id;
+         }
+    public void setId(Long id) { 
+        this.id = id;
+         }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public String getName() { 
+        return name;
+         }
+    public void setName(String name) 
+    { this.name = name;
+     }
 
-    public String getDescription() { return description; }
-    public void setDescription(String description) { this.description = description; }
+    public String getDescription() { 
+        return description; 
+        }
+    public void setDescription(String description) { 
+        this.description = description; 
+        }
 
-    public String getOpenTime() { return openTime; }
-    public void setOpenTime(String openTime) { this.openTime = openTime; }
+    public String getOpenTime() { 
+        return openTime;
+         }
+    public void setOpenTime(String openTime) { 
+        this.openTime = openTime; 
+        }
 
-    public String getCloseTime() { return closeTime; }
-    public void setCloseTime(String closeTime) { this.closeTime = closeTime; }
+    public String getCloseTime() {
+         return closeTime; 
+         }
+    public void setCloseTime(String closeTime) { 
+        this.closeTime = closeTime;
+         }
 }
